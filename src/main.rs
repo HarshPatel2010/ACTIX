26acff3defa41325d9079437e853605d5dcfa2cc
HarshPatel2010 <harshpatel29301@gmail.com>
2025-01-01 14:56:50 +0530

    bug solved reagarding engine

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn multiply(path:Path<(i64,i64)>)-> impl Responder {
     let mut engine = Engine::new();
     engine.register_fn("num1",move || num1);
     engine.register_fn("num2",move || num2);
-    let result = engine.eval_file::<i64>("src/multipply.rhai".into()).unwrap();
+    let result = engine.eval_file::<i64>("src/multiply.rhai".into()).unwrap();
     format!("multiply result {:?}", result)
 }
 #[get("/add/{num1}/{num2}")]
@@ -16,13 +16,13 @@ async fn add(path:Path<(i64,i64)>)-> impl Responder {
     let mut engine = Engine::new();
     engine.register_fn("num1", move || num1);
     engine.register_fn("num2", move || num2);
-    let result :i64 = engine.eval_file::<i64>("src/add/rhai.rhai".into()).unwrap();
+    let result :i64 = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
     format!("add result {:?}", result)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {App::new().service(multiply).service(add)}).bind(("127.0.0.1"),8000).unwrap().run().await
+    HttpServer::new(|| {App::new().service(add).service(multiply)}).bind(("127.0.0.1",8080)).unwrap().run().await
 
 }
 
